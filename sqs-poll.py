e7a2a4783a7657744c8f2351c1f3d9141a2b2a3a
castor-morveer <kbelov@ixiacom.com>
2016-07-07 16:10:02 -0700

    Update sqs-poll.py

--- sqs-poll.py
+++ sqs-poll.py
@@ -2,12 +2,19 @@
 import boto3
 import time
 import json
+import sys
 
-l = boto3.client('lambda', region_name='us-west-2')
-print("starting endless loop...")
+def main():
+  stage_label = sys.argv[1]
+  l = boto3.client('lambda', region_name='us-west-2')
+  print("starting endless loop...")
 
-while(True):
-  time.sleep(10)
-  res = l.invoke_async(FunctionName='echoTest', InvokeArgs=json.dumps({"ping":"pong"}))
-  print(res)
+  while(True):
+    time.sleep(10)
+    res = l.invoke_async(FunctionName='{0}_echoTest'.format(stage_label), InvokeArgs=json.dumps({"ping":"pong"}))
+    print(res)
+    
+if __name__ == '__main__':
+  sys.exit(main())
+  
 
