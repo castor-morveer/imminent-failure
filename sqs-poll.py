dd8ca0d92b9c49aa12c82e056222fefca6b9b418
castor-morveer <kbelov@ixiacom.com>
2016-07-07 16:12:38 -0700

    Update sqs-poll.py

--- sqs-poll.py
+++ sqs-poll.py
@@ -6,12 +6,13 @@ import sys
 
 def main():
   stage_label = sys.argv[1]
+  lambda_name = sys.argv[2]
   l = boto3.client('lambda', region_name='us-west-2')
   print("starting endless loop...")
 
   while(True):
     time.sleep(10)
-    res = l.invoke_async(FunctionName='{0}_echoTest'.format(stage_label), InvokeArgs=json.dumps({"ping":"pong"}))
+    res = l.invoke_async(FunctionName='{0}_{1}'.format(stage_label, lambda_name), InvokeArgs=json.dumps({"ping":"pong"}))
     print(res)
     
 if __name__ == '__main__':
